42b7a5660c19e161d381318757995657984f2960
Sumiati <umiakhi@gmail.com>
2019-06-11 05:20:06 +0700

    new controllers and views

--- application/controllers/Nilai.php
+++ application/controllers/Nilai.php
@@ -1,16 +1,18 @@
 <?php
 
 class Nilai extends CI_Controller{
- 
+
 	function __construct(){
 		parent::__construct();
-	
+		$this->load->model("m_nilai");
+
 		if($this->session->userdata('status') != "login"){
 			redirect(base_url("login"));
 		}
 	}
- 
+
 	function index(){
-		$this->load->view('data-nilai');
+		$data["nilai"] = $this->m_nilai->get_by_role();
+		$this->load->view('data-nilai', $data);
 	}
 }
