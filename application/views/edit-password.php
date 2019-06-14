85f6cbfc71d7579b0ae34c231f98297f367ef22c
Sumiati <umiakhi@gmail.com>
2019-06-14 17:03:09 +0700

    new views

---
+++ application/views/edit-password.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<?php $this->load->view("partial/head.php") ?>
+<form action="#" method="POST" name="form-ganti-password" enctype="multipart/form-data">
+    <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+        <tr height="56" align="center">
+            <td><font size="2" color="FFA800"><b>FORM GANTI PASSWORD PHP</b></font></td>
+        </tr>
+    </table>
+    <table width="75%" border="0" align="center" cellpadding="0" cellspacing="0">
+        <tr height="36">
+            <td width="25%">Username</td>
+            <td width="75%"><b><?=$username?><input type="hidden" name="username" id="username" value="<?=$username?>"></b></td>
+        </tr>
+        <tr height="36">
+            <td>Password Lama</td>
+            <td><input type="password" name="password_lama" id="password_lama" size="30" maxlength="20"></td>
+        </tr>
+        <tr height="36">
+            <td>Password Baru</td>
+            <td><input type="password" name="password_baru" id="password_baru" size="30" maxlength="20"></td>
+        </tr>
+        <tr height="36">
+            <td>Konfirm Password Baru</td>
+            <td><input type="password" name="konf_password" id="konf_password" size="30" maxlength="20"></td>
+        </tr>
+        <tr height="56">
+            <td> </td>
+            <td><input type="submit" name="Ganti" value="Ganti"></td>
+        </tr>
+    </table>
+</form>
+
+<?php $this->load->view("partial/foot.php") ?>
+</html>
---
+++ application/views/edit-user.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<?php $this->load->view("partial/head.php") ?>
+<div class="container-fluid">
+
+            <!-- Breadcrumbs-->
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item">
+                <a href="admin">Home</a>
+              </li>
+              <li class="breadcrumb-item active">Tables</li>
+            </ol>
+            <form method="POST" action="input_user2" enctype="multipart/form-data" class="col-md-12">
+              <div class="row">
+                <div class="col-md-4">                
+                  
+                  <div class="form-group">
+                    <label for="Username">Username (username berdasarkan nomor induk)</label>
+                    <select class="form-control" name="username">                      
+                        <option>Silahkan Pilih Siswa</option>           
+                      </select>
+                  </div>
+                 
+                  <div class="form-group">
+                    <label for="Password Orang Tua">Password</label>
+                    <input value=""  type="text" class="form-control" name="wali">
+                  </div>                   
+        
+                   <div class="form-group">
+                    <label for="Password Orang Tua">Akses</label>
+                    <input value=""  type="text" class="form-control" name="hp">
+                  </div> 
+                </div>
+              </div>
+
+              <div class="card-footer small text-mutedtext-center">
+                <?php 
+                if(isset($_GET['ubah'])){
+                 echo "<button type='submit' name='ubah' class='btn btn-warning col-md-2 col-xs-12'>Ubah</button>";
+               } 
+               else {
+                 echo "<button name='simpan perubahan' type='submit' class='btn btn-primary col-md-2 col-xs-12'>Simpan Perubahan</button> ";
+               }
+               ?>
+             </div>
+
+           </form>
+           <p class="small text-center text-muted m    y-5">
+            <em></em>
+          </p>
+
+        </div>
+
+<?php $this->load->view("partial/foot.php") ?>
+</html>
